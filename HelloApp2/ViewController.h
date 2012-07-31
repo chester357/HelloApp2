0d2c3015db0e00064ee23d4fe0210ea3f45d6605
Chester Schofield <chesterschofield@yahoo.com>
2012-07-31 13:40:55 -0500

    Initial Commit

---
+++ HelloApp2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloApp2
+//
+//  Created by MBP on 7/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
